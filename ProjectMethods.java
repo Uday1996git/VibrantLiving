816f302cd4fcc245e70fe5cfe63680749002eebc
Uday1996git <uday.janoos@gmail.com>
2022-05-29 01:19:14 +0530

    Changes to Project Nethods

--- ProjectMethods.java
+++ ProjectMethods.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
+//This class contains all the methods to build the project
 public class ProjectMethods extends BaseMethods {
 	static String Main;
 	static Actions builder;
