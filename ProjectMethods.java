42bf01233b06d1b83e6797adcab11aa862321d2b
Uday1996git <uday.janoos@gmail.com>
2022-06-29 22:37:03 +0530

    2nd commit today

--- ProjectMethods.java
+++ ProjectMethods.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
+// we have all the project methods in this class, Please extend this class to create test cases.
 public class ProjectMethods extends BaseMethods {
 	static String Main;
 	static Actions builder;
